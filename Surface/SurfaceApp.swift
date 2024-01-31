ebcc5f5e7114cdf7c639ff039688608c9697c442
Etienne Vautherin <etienne@vautherin.com>
2024-01-31 11:49:38 +0100

    Initial Commit

---
+++ Surface/SurfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfaceApp.swift
+//  Surface
+//
+//  Created by Etienne Vautherin on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SurfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
